563b626ff6add7ee08afdff55cce9abe53156a4a
shshshee <sehhe22@naver.com>
2021-02-02 23:13:07 +0900

    boj2156

---
+++ boj2156.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class boj2156 {
+	// 2156 포도주 시식 
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n+1]; 
+		int[] dp = new int[n+1];
+		
+		//입력받기
+		for(int i=1;i<=n;i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		dp[1] = arr[1];
+		
+		if(n>=2) {
+			dp[2] = arr[1] + arr[2];
+		}
+		
+		//dp
+		for(int i=3;i<=n;i++) {
+			dp[i] = Math.max(dp[i-2]+arr[i], dp[i-3]+arr[i-1]+arr[i]);
+			dp[i] = Math.max(dp[i],dp[i-1]);
+		}
+		
+		System.out.println(dp[n]);
+		
+		
+		
+	}
+}
