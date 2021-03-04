6866632da4128eeb4d97a8d949bd5542dd2f83dd
shshshee <sehhe22@naver.com>
2021-03-05 00:51:18 +0900

    구명보트_programmers

---
+++ 구명보트.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people); //정렬 
+        
+        int min = 0;
+        for(int i=people.length-1;i>=min;i--){
+            if(people[min]+people[i]<=limit) {
+                min ++; 
+            } 
+            answer ++;
+        }
+        
+        return answer;
+    }
+}
